cc519630dab492294fc88034aaeaae7eab1cb197
nermakovaa <nadezhda.ermakova.99@gmail.com>
2024-06-10 15:29:01 +0300

    Update class model

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from transformers import pipeline
+import networkx as nx
 
 
 class Model:
@@ -10,7 +11,18 @@ class Model:
         '''
         Доля подписчиков сообщества среди активных пользователей
         '''
-        pass
+        active_users = 0
+        followers = 0
+
+        for post in self.data['vk'][0]['posts']:
+            if post['from'] != None:
+                active_users += 1
+                followers += post['from']['is_member']
+                
+        if active_users != 0:
+            return followers / active_users
+        else:
+            return 0
     
     def user_engagement_ratio(self):
         '''
@@ -42,9 +54,24 @@ class Model:
     
     def top_audience_ratio(self):
         '''
-        Доля подписчиков из топ 100 активных пользователей
+        Доля подписчиков из топа активных пользователей
         '''
-        pass
+        active_users = 0
+        followers = 0
+
+        for post in self.data['vk'][0]['posts']:
+            if post['from'] != None:
+                sorted_posts = sorted(self.data['vk'][0]['posts'], key=lambda x: (x['views'] if x['views'] is not None else len(x['replies'])) if x['from'] is not None else -1, reverse=True)
+                top_posts = sorted_posts[:10]
+
+        for post in top_posts:
+            active_users += 1
+            followers += post['from']['is_member']
+                
+        if active_users != 0:
+            return followers / active_users
+        else:
+            return 0
     
     def love_rate(self):
         '''
@@ -64,13 +91,13 @@ class Model:
                 post_count += 1
 
         if post_count != 0:
-            return love_rate_sum / post_count
+            return (love_rate_sum / post_count) / 0.05
         else:
             return 0
             
     def trending_content_sentiment_ratio(self):
         '''
-        Доля позитивных комментариев из топ 10 наиболее обсуждаемых постов
+        Доля позитивных комментариев из топа наиболее обсуждаемых постов
         '''
         posts_with_replies = []
 
@@ -157,17 +184,60 @@ class Model:
         else:
             return 0
         
-    def brand_response_to_comments():
+    def brand_response_to_comments(self):
         '''
         Среднее время отклика компании на комментарии
         '''
-        pass
+        count_1 = 0
+        count_0 = 0
+
+        for post in self.data['vk'][0]['posts']:
+            text = post['text'].lower()
+            brand_name = self.data['vk'][0]['groupName'].lower()
+            
+            if brand_name in text:
+                for reply in post['replies']:
+                    if reply.get('sender_name', '').lower() in brand_name or reply.get('last_name', '').lower() in brand_name:
+                        count_1 += 1
+                        break
+                else:
+                    count_0 += 1
+
+        total_posts = count_1 + count_0
+
+        if total_posts != 0:
+            return (count_1 + count_0) / total_posts
+        else:
+            return 0
         
-    def brand_reaction_to_negativity():
-        '''
-        Среднее время отклика бренда на негативные комментарии
-        '''
-        pass
+    def brand_reaction_to_negativity(self):
+        count_1 = 0
+        count_0 = 0
+
+        classifier = pipeline("sentiment-analysis", model="blanchefort/rubert-base-cased-sentiment")
+
+        for post in self.data['vk'][0]['posts']:
+            text = post['text'].lower()
+            brand_name = self.data['vk'][0]['groupName'].lower()
+            
+            if brand_name in text:
+                result = classifier(text)
+                tone = result[0]['label']
+
+                if tone == 'NEGATIVE':
+                    for reply in post['replies']:
+                        if reply.get('sender_name', '').lower() in brand_name or reply.get('last_name', '').lower() in brand_name:
+                            count_1 += 1
+                            break
+                    else:
+                        count_0 += 1
+
+        total_posts = count_1 + count_0
+
+        if total_posts != 0:
+            return (count_1 + count_0) / total_posts
+        else:
+            return 0
         
     def brand_responsiveness(self):
         '''
@@ -196,7 +266,78 @@ class Model:
         '''
         Доля лояльных инфлюенсеров из топа
         '''
-        pass
+        connections = []
+
+        for post in self.data['vk'][0]['posts']:
+            if post['from'] != None:
+                post_connections = [(post['from']['id'], reply['sender_id']) for reply in post['replies']]
+                connections.extend(post_connections)
+
+        G = nx.DiGraph()
+        G.add_edges_from(connections)
+        pos = nx.spring_layout(G, k=0.15, iterations=20) 
+
+        # pagerank - https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.link_analysis.pagerank_alg.pagerank.html
+        pr = nx.pagerank(G)
+        sorted_pr = sorted(pr.items(), key=lambda x: x[1], reverse=True)
+
+        top_10_pr = sorted_pr[:10] 
+        top_10_ids_pr = [id for id, pr in top_10_pr]
+
+        # https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.centrality.betweenness_centrality.html
+        centrality = nx.betweenness_centrality(G)
+        sorted_centrality = sorted(centrality.items(), key=lambda x: x[1], reverse=True)
+
+        top_10_centrality = sorted_centrality[:10] 
+        top_10_ids_centrality = [id for id, _ in top_10_centrality]
+
+        top_users = list(set(top_10_ids_pr + top_10_ids_centrality))
+
+        classifier = pipeline("sentiment-analysis", model="blanchefort/rubert-base-cased-sentiment")
+
+        user_sentiments = {}
+
+        for post in self.data['vk'][0]['posts']:
+            if post['from'] is not None and post['from']['id'] in top_users:
+                text = post['text']
+                result = classifier(text)[0]
+
+                user_id = post['from']['id']
+                if user_id not in user_sentiments:
+                    user_sentiments[user_id] = {"POSITIVE": 0, "NEGATIVE": 0}
+
+                if result['label'] == 'POSITIVE':
+                    user_sentiments[user_id]["POSITIVE"] += 1
+                elif result['label'] == 'NEGATIVE':
+                    user_sentiments[user_id]["NEGATIVE"] += 1
+
+            for reply in post['replies']:
+                if reply['sender_id'] in top_users:
+                    text = reply['text']
+                    result = classifier(text)[0]
+
+                    user_id = reply['sender_id']
+                    if user_id not in user_sentiments:
+                        user_sentiments[user_id] = {"POSITIVE": 0, "NEGATIVE": 0}
+
+                    if result['label'] == 'POSITIVE':
+                        user_sentiments[user_id]["POSITIVE"] += 1
+                    elif result['label'] == 'NEGATIVE':
+                        user_sentiments[user_id]["NEGATIVE"] += 1
+
+        loyalty_scores = {}
+
+        for user_id, sentiment_info in user_sentiments.items():
+            positive = sentiment_info['POSITIVE']
+            negative = sentiment_info['NEGATIVE']
+
+            loyalty_score = positive / (positive + negative) if (positive + negative) > 0 else 0
+            loyalty_scores[user_id] = loyalty_score
+
+        if len(loyalty_scores.values()) != 0:
+            return sum(loyalty_scores.values()) / len(loyalty_scores.values())
+        else:
+            return 0
             
     def channel_citation_index(self):
         '''
@@ -216,6 +357,28 @@ class Model:
                 post_count += 1
 
         if post_count != 0:
-            return channel_citation_sum / post_count
+            return (channel_citation_sum / post_count) / 0.05
         else:
-            return 0  
+            return 0
+        
+    def calculate_model(self):
+        '''
+        Итоговые значения
+        '''
+        name_groups = ['Показатели вовлеченности аудитории', 'Метрики для оценивания обратной связи аудитории',
+                       'Метрики эффективности коммуникаций бренда', 'Метрики для оценивания взаимодействия с инфлюенсерами',
+                       'Метрики информационного присутствия бренда']
+        groups = [
+            [self.brand_follower_ratio, self.user_engagement_ratio, self.top_audience_ratio], # Показатели вовлеченности аудитории
+            [self.love_rate, self.trending_content_sentiment_ratio, self.net_promoter_score], # Метрики для оценивания обратной связи аудитории
+            [self.brand_response_to_comments, self.brand_reaction_to_negativity, self.brand_responsiveness], # Метрики эффективности коммуникаций бренда
+            [self.influencer_sentiment_ratio], # Метрики для оценивания взаимодействия с инфлюенсерам
+            [self.channel_citation_index] # Метрики информационного присутствия бренда
+        ]
+
+        group_sums = [sum(metric() for metric in group) for group in groups]
+        average = sum(group_sums) / 11
+
+        min_index = group_sums.index(min(group_sums))
+
+        return average, name_groups[min_index] # average - итоговый рейтинг, название группы слабой группы 
