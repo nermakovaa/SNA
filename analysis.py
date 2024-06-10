673db7bb676f5344f50a2c1add40a51d0f8e95fd
nermakovaa <nadezhda.ermakova.99@gmail.com>
2024-06-11 01:25:37 +0300

    Update class PageRank and class BetweennessCentralityRank

--- analysis.py
+++ analysis.py
@@ -5,6 +5,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import json
 import emoji
+import os
 
 
 class Telegram:
@@ -443,102 +444,332 @@ class Vkontakte:
         return top_emojis  
     
 
-class General():  
-    def __init__(self, data):
+class PageRank:
+    '''
+    Топ-10 авторов с наибольшей способностью вовлекать других пользователей в дискуссию
+    [пользователь | значение page rank]
+    '''
+    def __init__(self, data, top_authors=10):
         self.data = data
-      
-    def page_rank(self, sender_id, data, connections):
-        '''
-        Топ-10 авторов с наибольшей способностью вовлекать других пользователей в дискуссию
-        [пользователь | значение pagerank]
+        self.top_authors = top_authors
+        self.sender_id, self.sender_info, self.connections = self.__get_data()
+        self.__get_file()
         
-        sender_id - nodes (id) example [108718, 108719, 108720, 108721, ]
-        data - (id: {first_name: , last_name: }) example {108718: {'first_name': 'Richard', 'last_name': 'Lowe'}, }
-        connections - edges (one-to-one = id-to-id) example [(108990, 108928), (108916, 108760), ]
+    def __get_data(self):
+        '''
+        sender_id - узлы (id пользователей)
+        sender_info - (словарь с id в качестве ключа и значений в качестве словаря из first_name и last_name)
+        connections - ребра (id-to-id)
+        '''
+        connections = []
+
+        for post in self.data['vk'][0]['posts']:
+            if post['from'] != None:
+                post_connections = [(post['from']['id'], reply['sender_id']) for reply in post['replies']]
+                connections.extend(post_connections)
+
+        sender_id = list(set(sum(connections, ())))
+
+        sender_info = {}
+
+        for post in self.data['vk'][0]['posts']:
+            if post['from'] is not None and post['from']['id'] in sender_id:
+                if post['from']['id'] not in sender_info:
+                    sender_info[post['from']['id']] = {
+                        'first_name': post['from']['first_name'],
+                        'last_name': post['from']['last_name']
+                    }
+
+        for post in self.data['vk'][0]['posts']:
+            if 'replies' in post:
+                for reply in post['replies']:
+                    if reply['sender_id'] in sender_id:
+                        if reply['sender_id'] not in sender_info:
+                            sender_info[reply['sender_id']] = {
+                                'first_name': reply['sender_name'],
+                                'last_name': reply['last_name']
+                            }
+
+        return sender_id, sender_info, connections
+
+    def page_rank(self):
+        '''
+        Для получения топа пользователей pagerank {722219350: 0.04746, 732871646: 0.03822 ...}
         '''
         G = nx.DiGraph()
-        [G.add_node(k, first_name = data[k]['first_name'], last_name = data[k]['last_name']) for k in sender_id]
-        G.add_edges_from(connections)
-        # spring_layout - https://networkx.org/documentation/stable/reference/generated/networkx.drawing.layout.spring_layout.html
+        [G.add_node(k, first_name = self.sender_info[k]['first_name'], last_name = self.sender_info[k]['last_name']) for k in self.sender_id]
+        G.add_edges_from(self.connections)
         pos = nx.spring_layout(G, k=0.15, iterations=20) 
-
+        
         # pagerank - https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.link_analysis.pagerank_alg.pagerank.html
         pr = nx.pagerank(G)
         sorted_pr = sorted(pr.items(), key=lambda x: x[1], reverse=True)
 
-        top_10 = sorted_pr[:10] 
-        top_10_ids = [id for id, pr in top_10]
+        top_pr = sorted_pr[:self.top_authors] 
+        
         # топ 10 пользователей с наибольшим pagerank
-        top_nodes = {node: G.nodes[node]['first_name'] + ' ' + G.nodes[node]['last_name'] for node in dict(top_10).keys()} 
-
-        # для графа
-        node_colors = ['#FF5558' if node in top_10_ids else "#27BBBD" for node in G.nodes()]
-        # plt.figure(figsize = (10, 10)) 
-        # nx.draw_networkx_labels(G, pos, 
-        #                         labels = top_nodes, 
-        #                         font_color = '#333335', 
-        #                         font_size = 10, 
-        #                         bbox = dict(facecolor='white', edgecolor='white', boxstyle='square'))
-        # nx.draw(G, pos, 
-        #         nodelist = list(pr.keys()), 
-        #         node_size = [v * 100000 for v in pr.values()], 
-        #         with_labels = False, 
-        #         node_color = node_colors, 
-        #         edge_color = '#27BBBD', 
-        #         width = 0.2)
-        # plt.show()
-
-        # для таблицы "Top 20 influencers by PageRank"
-        name_surname_dict = {data[k]['first_name'] + ' ' + data[k]['last_name']: round(v, 5) for k, v in top_10}
-        return name_surname_dict
-        # print(name_surname_dict)
+        name_surname_dict = {k: round(v, 5) for k, v in top_pr}
         
-    def betweenness_centrality_rank(self, sender_id, example_data, connections):
+        return name_surname_dict # возвращает id и значение pagerank {722219350: 0.04746, 732871646: 0.03822 ...}
+    
+    def __get_file(self):
         '''
-        Топ-10 авторов с наибольшей способностью связывать подсети других пользователей в графе
-        [пользователь | значение betweenness centrality rank]
-        
-        sender_id - узлы (id пользователей)
-        example_data - (словарь с id в качестве ключа и значений в качестве словаря из first_name и last_name)
-        connections - ребра (one-to-one = id-to-id)
+        Для сохранения файла pagerank
         '''
-        G = nx.DiGraph()
-        [G.add_node(k, first_name = example_data[k]['first_name'], last_name = example_data[k]['last_name']) for k in sender_id]
-        G.add_edges_from(connections)
         # spring_layout - https://networkx.org/documentation/stable/reference/generated/networkx.drawing.layout.spring_layout.html
+        G = nx.DiGraph()
+        [G.add_node(k, first_name=self.sender_info[k]['first_name'], last_name=self.sender_info[k]['last_name']) for k in self.sender_id]
+        G.add_edges_from(self.connections)
         pos = nx.spring_layout(G, k=0.15, iterations=20)
 
-        # https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.centrality.betweenness_centrality.html
+        pr = nx.pagerank(G)
+        sorted_pr = sorted(pr.items(), key=lambda x: x[1], reverse=True)
+        top_pr = sorted_pr[:self.top_authors]
+        top_ids = [id for id, pr in top_pr]
+        top_nodes = {node: G.nodes[node]['first_name'] + ' ' + G.nodes[node]['last_name'] for node in dict(top_pr).keys()}
+
+        node_colors = ['#FF5558' if node in top_ids else "#27BBBD" for node in G.nodes()]
+        plt.figure(figsize=(10, 10))
+        nx.draw_networkx_labels(G, pos,
+                                labels=top_nodes,
+                                font_color='#333335',
+                                font_size=10,
+                                bbox=dict(facecolor='white', edgecolor='white', boxstyle='square'))
+        nx.draw(G, pos,
+                nodelist=list(pr.keys()),
+                node_size=[v * 20000 for v in pr.values()],
+                with_labels=False,
+                node_color=node_colors,
+                edge_color='#27BBBD',
+                width=0.2)
+        
+        current_dir = os.getcwd()
+        file_path = os.path.join(current_dir, 'data/PageRank.png')
+        plt.savefig(file_path) 
+        plt.close()  
+        
+    def get_table(self):
+        '''
+        Данные для таблицы топ-инфлюенсеров
+        '''
+        result_dict = {}
+        for k, v in self.page_rank().items():
+            name = self.sender_info[k]['first_name'] + ' ' + self.sender_info[k]['last_name']
+            result_dict[name] = {
+                'page_rank': self.page_rank()[k],
+                'id': k
+            }
+
+        for key, value in result_dict.items():
+            id_to_find = value['id']
+            count = 0
+            for tpl in self.connections:
+                if id_to_find in tpl:
+                    count += 1
+            value['engagement_users'] = count
+        
+        for key, value in result_dict.items():
+            value['comments'] = []  # создаем пустой список для комментариев
+            
+        id = [value['id'] for key, value in result_dict.items()]
+
+        for post in self.data['vk'][0]['posts']:
+            for replies in post['replies']:
+                for key, value in result_dict.items():
+                    if replies['sender_id'] == value['id']:
+                        value['comments'].append(replies['text'])
+                        
+        classifier = pipeline("sentiment-analysis", model="blanchefort/rubert-base-cased-sentiment")
+        
+        for key, value in result_dict.items():
+            pos_count = 0
+            neg_count = 0
+            comments = value['comments']
+
+            for comment in comments:
+                sentiment = classifier(comment)
+                if sentiment[0]['label'] == 'POSITIVE':
+                    pos_count += 1
+                else:
+                    neg_count += 1
+
+            if (pos_count + neg_count) != 0:
+                nps = (pos_count / (pos_count + neg_count)) * 100 # nps = лояльность
+                value['net_promoter_score'] = nps
+            else:
+                0     
+                                    
+        for key in result_dict:
+            if 'comments' in result_dict[key]:
+                del result_dict[key]['comments']
+        
+        return result_dict # {'Марина Вкусвилл': {'page_rank': 0.04746,  'id': 722219350,  'engagement_users': 28,  'net_promoter_score': 82.14285714285714},
+
+
+class BetweennessCentralityRank:
+    '''
+    Топ-10 авторов с наибольшей способностью вовлекать других пользователей в дискуссию
+    [пользователь | значение page rank]
+    '''
+    def __init__(self, data, top_authors=10):
+        self.data = data
+        self.top_authors = top_authors
+        self.sender_id, self.sender_info, self.connections = self.__get_data()
+        self.__get_file()
+        
+    def __get_data(self):
+        '''
+        sender_id - узлы (id пользователей)
+        sender_info - (словарь с id в качестве ключа и значений в качестве словаря из first_name и last_name)
+        connections - ребра (id-to-id)
+        '''
+        connections = []
+
+        for post in self.data['vk'][0]['posts']:
+            if post['from'] != None:
+                post_connections = [(post['from']['id'], reply['sender_id']) for reply in post['replies']]
+                connections.extend(post_connections)
+
+        sender_id = list(set(sum(connections, ())))
+
+        sender_info = {}
+
+        for post in self.data['vk'][0]['posts']:
+            if post['from'] is not None and post['from']['id'] in sender_id:
+                if post['from']['id'] not in sender_info:
+                    sender_info[post['from']['id']] = {
+                        'first_name': post['from']['first_name'],
+                        'last_name': post['from']['last_name']
+                    }
+
+        for post in self.data['vk'][0]['posts']:
+            if 'replies' in post:
+                for reply in post['replies']:
+                    if reply['sender_id'] in sender_id:
+                        if reply['sender_id'] not in sender_info:
+                            sender_info[reply['sender_id']] = {
+                                'first_name': reply['sender_name'],
+                                'last_name': reply['last_name']
+                            }
+
+        return sender_id, sender_info, connections
+
+    def page_rank(self):
+        '''
+        Для получения топа пользователей betweenness_centrality_rank {722219350: 0.04746, 732871646: 0.03822 ...}
+        '''
+        G = nx.DiGraph()
+        [G.add_node(k, first_name = self.sender_info[k]['first_name'], last_name = self.sender_info[k]['last_name']) for k in self.sender_id]
+        G.add_edges_from(self.connections)
+        pos = nx.spring_layout(G, k=0.15, iterations=20) 
+        
+        # pagerank - https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.link_analysis.pagerank_alg.pagerank.html
         centrality = nx.betweenness_centrality(G)
         sorted_centrality = sorted(centrality.items(), key=lambda x: x[1], reverse=True)
 
-        # топ 10 пользователей с наибольшим pagerank
-        top_10 = sorted_centrality[:10] 
-        top_10_ids = [id for id, _ in top_10]
-        top_nodes = {node: G.nodes[node]['first_name'] + ' ' + G.nodes[node]['last_name'] for node in dict(top_10).keys()} 
-
-        # для графа
-        node_colors = ['#FF5558' if node in top_10_ids else '#27BBBD' for node in G.nodes()]
-        # plt.figure(figsize = (10, 10)) 
-        # nx.draw_networkx_labels(G, pos, 
-        #                         labels = top_nodes, 
-        #                         font_color = '#333335', 
-        #                         font_size = 10, 
-        #                         bbox = dict(facecolor='white', edgecolor='white', boxstyle='square'))
-        # nx.draw(G, pos, 
-        #         nodelist = list(centrality.keys()), 
-        #         node_size = [v * 300000 for v in centrality.values()], 
-        #         with_labels = False, 
-        #         node_color = node_colors, 
-        #         edge_color = '#27BBBD', 
-        #         width = 0.2)
-        # plt.show()
-
-        # для таблицы "Top 20 influencers by Betweenness Centrality Rank"
-        name_surname_dict = {example_data[k]['first_name'] + ' ' + example_data[k]['last_name']: round(v, 5) for k, v in top_10}
-        return name_surname_dict
-        # print(name_surname_dict)
+        top_centrality = sorted_centrality[:self.top_authors] 
         
+        # топ 10 пользователей с наибольшим pagerank
+        name_surname_dict = {k: round(v, 5) for k, v in top_centrality}
+        
+        return name_surname_dict # возвращает id и значение pagerank {722219350: 0.04746, 732871646: 0.03822 ...}    
+    
+    def __get_file(self):
+        '''
+        Для сохранения файла pagerank
+        '''
+        # spring_layout - https://networkx.org/documentation/stable/reference/generated/networkx.drawing.layout.spring_layout.html
+        G = nx.DiGraph()
+        [G.add_node(k, first_name=self.sender_info[k]['first_name'], last_name=self.sender_info[k]['last_name']) for k in self.sender_id]
+        G.add_edges_from(self.connections)
+        pos = nx.spring_layout(G, k=0.15, iterations=20)
+
+        centrality = nx.betweenness_centrality(G)
+        sorted_centrality = sorted(centrality.items(), key=lambda x: x[1], reverse=True)
+        top_centrality = sorted_centrality[:self.top_authors] 
+        top_ids = [id for id, pr in top_centrality]
+        top_nodes = {node: G.nodes[node]['first_name'] + ' ' + G.nodes[node]['last_name'] for node in dict(top_centrality).keys()}
+
+        node_colors = ['#FF5558' if node in top_ids else '#27BBBD' for node in G.nodes()]
+        plt.figure(figsize=(15, 15))
+        nx.draw_networkx_labels(G, pos,
+                                labels=top_nodes,
+                                font_color='#333335',
+                                font_size=10,
+                                bbox=dict(facecolor='white', edgecolor='white', boxstyle='square'))
+        nx.draw(G, pos,
+                nodelist=list(centrality.keys()),
+                node_size=[v * 20000 for v in centrality.values()],
+                with_labels=False,
+                node_color=node_colors,
+                edge_color='#27BBBD',
+                width=0.2)
+        
+        current_dir = os.getcwd()
+        file_path = os.path.join(current_dir, 'data/BetweennessCentralityRank.png')
+        plt.savefig(file_path) 
+        plt.close()      
+    
+    def get_table(self):
+        '''
+        Данные для таблицы топ-инфлюенсеров BetweennessCentralityRank
+        '''
+        result_dict = {}
+        for k, v in self.page_rank().items():
+            name = self.sender_info[k]['first_name'] + ' ' + self.sender_info[k]['last_name']
+            result_dict[name] = {
+                'page_rank': self.page_rank()[k],
+                'id': k
+            }
+
+        for key, value in result_dict.items():
+            id_to_find = value['id']
+            count = 0
+            for tpl in self.connections:
+                if id_to_find in tpl:
+                    count += 1
+            value['engagement_users'] = count
+        
+        for key, value in result_dict.items():
+            value['comments'] = []  
+
+        for post in self.data['vk'][0]['posts']:
+            for replies in post['replies']:
+                for key, value in result_dict.items():
+                    if replies['sender_id'] == value['id']:
+                        value['comments'].append(replies['text'])
+                        
+        classifier = pipeline("sentiment-analysis", model="blanchefort/rubert-base-cased-sentiment")
+        
+        for key, value in result_dict.items():
+            comments = value['comments']
+            
+            pos_count = 0
+            neg_count = 0
+            neu_count = 0
+
+            for comment in comments:
+                sentiment = classifier(comment)
+                label = sentiment[0]['label']
+
+                if label == 'POSITIVE':
+                    pos_count += 1
+                elif label == 'NEGATIVE':
+                    neg_count += 1
+                else:
+                    neu_count += 1
+            
+            total_count = pos_count + neg_count + neu_count
+            value['positive'] = pos_count / total_count * 100
+            value['negative'] = neg_count / total_count * 100
+            value['neutral'] = neu_count / total_count * 100
+            
+        for key in result_dict:
+            if 'comments' in result_dict[key]:
+                del result_dict[key]['comments']
+        
+        return result_dict # {'Татьяна Балакирева': {'page_rank': 0.07433, 'id': 588079193,  'engagement_users': 81,  'positive': 73.33333333333333,  'negative': 8.88888888888889,  'neutral': 17.77777777777778},
+            
         
 class InfluencerTable:
     '''
